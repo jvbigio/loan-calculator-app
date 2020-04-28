679f80f22ab2fc5a6bd599d77f11bb33301090bd
Joe Bigio <beej@Joes-MacBook-Pro.local>
2020-04-28 17:38:34 -0400

    loader img and results

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+  hideResults();
+  showLoader();
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e){
+function calculateResults(){
   console.log('Calculating...');
   // UI Vars
   const amount = document.getElementById('amount');
@@ -18,21 +25,24 @@ function calculateResults(e){
 
   // Compute monthly payment
   const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-  const monthly = (principal*x*calculatedInterest)/(x-1);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
 
   if(isFinite(monthly)) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-    totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    showResults();
+    hideLoader();
   } else {
     showError('Please check your numbers');
   }
-
-  e.preventDefault();
 }
 
 // Show Error
 function showError(error){
+  showResults();
+  hideLoader();
+
   // Create a div
   const errorDiv = document.createElement('div');
 
@@ -41,7 +51,7 @@ function showError(error){
   const heading = document.querySelector('.heading');
 
   // Add class
-  errorDiv.className = 'alert alert-danger';h
+  errorDiv.className = 'alert alert-danger';
 
   // Create text node and append to div
   errorDiv.appendChild(document.createTextNode(error));
@@ -57,4 +67,24 @@ function showError(error){
 // Clear error
 function clearError(){
   document.querySelector('.alert').remove();
+}
+
+// Show results
+const showResults = () => {
+  document.getElementById('results').style.display = 'block';
+}
+
+// Hide results
+const hideResults = () => {
+  document.getElementById('results').style.display = 'none';
+}
+
+// Show loader
+const showLoader = () => {
+  document.getElementById('loading').style.display = 'block';
+}
+
+// hide loader
+const hideLoader = () => {
+document.getElementById('loading').style.display = 'none';
 }
